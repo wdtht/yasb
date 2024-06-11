14c403d1b6e175edc2b5be94cd3d2cb0115306c6
xianfa <zeng-xian-fa@qq.com>
2024-06-11 20:07:04 +0800

    添加时间弹窗功能

--- src/core/widgets/any_content.py
+++ src/core/widgets/any_content.py
@@ -3,8 +3,9 @@ from abc import ABC, abstractmethod
 from PyQt6.QtGui import QKeyEvent, QTextCursor
 from core.widgets.base import BaseWidget
 from core.validation.widgets.example import EXAMPLE_VALIDATION_SCHEMA
-from PyQt6.QtWidgets import QLabel, QCheckBox, QTextEdit, QTextBrowser
-from PyQt6.QtCore import Qt, pyqtSignal, pyqtBoundSignal
+from PyQt6.QtWidgets import QLabel, QCheckBox, QTextEdit, QTextBrowser, QDialog, QVBoxLayout, QDateTimeEdit, \
+    QDialogButtonBox
+from PyQt6.QtCore import Qt, pyqtSignal, pyqtBoundSignal, QDateTime
 from rx import operators as ops
 from rx.subject import BehaviorSubject as LiveData
 
@@ -21,16 +22,16 @@ class AnyContentWidget(BaseWidget):
     ):
         super().__init__(update_interval, class_name="template-widget")
         self._task_list = [
-            {"task": "这里是第一个示例任务", "completed": False, "next_step": "", "all_step": ""},
-            {"task": "这里是第二个示例任务", "completed": False, "next_step": "", "all_step": ""},
-            {"task": "这里是第三个示例任务", "completed": False, "next_step": "", "all_step": ""},
-            {"task": "这里是第四个示例任务", "completed": False, "next_step": "", "all_step": ""},
-            {"task": "这里是第五个示例任务", "completed": False, "next_step": "", "all_step": ""},
-            {"task": "这里是第六个示例任务", "completed": False, "next_step": "", "all_step": ""},
-            {"task": "这里是第七个示例任务", "completed": False, "next_step": "", "all_step": ""},
-            {"task": "这里是第八个示例任务", "completed": False, "next_step": "", "all_step": ""},
-            {"task": "这里是第九个示例任务", "completed": False, "next_step": "", "all_step": ""},
-            {"task": "这里是第十个示例任务", "completed": False, "next_step": "", "all_step": ""}
+            {"task": "这里是第一个示例任务", "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 1},
+            {"task": "这里是第二个示例任务", "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 1},
+            {"task": "这里是第三个示例任务", "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 1},
+            {"task": "这里是第四个示例任务", "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 1},
+            {"task": "这里是第五个示例任务", "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 1},
+            {"task": "这里是第六个示例任务", "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 1},
+            {"task": "这里是第七个示例任务", "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 1},
+            {"task": "这里是第八个示例任务", "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 1},
+            {"task": "这里是第九个示例任务", "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 1},
+            {"task": "这里是第十个示例任务", "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 1}
         ]
         self._init_view()
         self._current_task_index = LiveData(-1)
@@ -39,6 +40,7 @@ class AnyContentWidget(BaseWidget):
         self._next_step_business()
         self._show_current_step_business()
         self._complete_current_task_business()
+        self._add_task_business()
 
         self._current_task_index.on_next(0)
 
@@ -86,8 +88,7 @@ class AnyContentWidget(BaseWidget):
         self._current_task_next_step.subscribe(
             lambda x: self._next_step.setPlainText(x))
 
-        def handle_next_step_input():
-            # 处理用户输入来跳过当前任务
+        def update_current_step_and_enter_next_task():
             input_text = self._next_step.toPlainText().strip()
             if input_text:
                 current_all_step = self._current_task_all_step.value
@@ -96,18 +97,49 @@ class AnyContentWidget(BaseWidget):
                 self._current_task_all_step.on_next(current_all_step + f"{input_text}")
                 self._enter_next_uncheck_task()
 
-        self._next_step.taskSubmitted.connect(handle_next_step_input)
-
         self._next_step.taskSubmitted.connect(
             lambda: self._current_task_next_step.on_next(self._next_step.toPlainText()))
 
         def update_current_task(index: int):
             if index == -1:
-                self._current_task_next_step.on_next("输入内容以添加新任务")
+                self._current_task_next_step.on_next("默认任务没有下一步计划")
             else:
                 self._current_task_next_step.on_next(self._task_list[index]["next_step"])
 
         self._current_task_index.pipe(ops.distinct_until_changed()).subscribe(lambda x: update_current_task(x))
+
+        # 构造并弹出时间选择器, 并在用户选择时间后更新下一步计划
+        def show_time_picker():
+            dialog = QDialog()
+            dialog.setWindowTitle("选择时间")
+
+            # 设置布局和时间选择控件
+            layout = QVBoxLayout()
+            time_picker = QDateTimeEdit()
+            time_picker.setCalendarPopup(True)
+            time_picker.setDateTime(QDateTime.currentDateTime())
+            time_picker.setMinimumDateTime(QDateTime.currentDateTime())
+            time_picker.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
+            layout.addWidget(time_picker)
+
+            # 添加按钮
+            button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
+            button_box.accepted.connect(dialog.accept)
+            button_box.rejected.connect(dialog.reject)
+            layout.addWidget(button_box)
+
+            dialog.setLayout(layout)
+
+            def update_current_and_enter_next_task():
+                self._task_list[self._current_task_index.value]["remind_time"] = time_picker.dateTime().toString("yyyy-MM-dd HH:mm:ss")
+                self._task_list[self._current_task_index.value]["priority"] = -1
+                update_current_step_and_enter_next_task()
+
+            dialog.accepted.connect(update_current_and_enter_next_task)
+
+            dialog.exec()
+
+        self._next_step.taskSubmitted.connect(show_time_picker)
 
     def _show_current_task_title_business(self):
         self._current_task_title.pipe(ops.distinct_until_changed()).subscribe(lambda x: self._label.setText(x))
@@ -165,6 +197,21 @@ class AnyContentWidget(BaseWidget):
         self._next_step.setProperty("class", "next-step")
         self._next_step.setPlaceholderText("输入下一步计划以跳过当前任务")
         self.widget_layout.addWidget(self._next_step)
+
+        self._add_task = TaskTextEdit(self)
+        self._add_task.setProperty("class", "add-task")
+        self._add_task.setPlaceholderText("输入标题以快速创建新任务")
+        self.widget_layout.addWidget(self._add_task)
+
+    def _add_task_business(self):
+        def handle_add_task_input():
+            input_text = self._add_task.toPlainText().strip()
+            if input_text:
+                self._task_list.append({"task": input_text, "completed": False, "next_step": "", "all_step": "", "remind_time": "", "priority": 0})
+                self._current_task_index.on_next(len(self._task_list) - 1)
+                self._add_task.clear()
+
+        self._add_task.taskSubmitted.connect(handle_add_task_input)
 
 
 class TaskTextEdit(QTextEdit):
